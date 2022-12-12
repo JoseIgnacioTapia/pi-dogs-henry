b0998bb74f302c322b906249c82c532f2a04d21d
JoeIt <josignaciot@gmail.com>
2022-12-11 21:28:02 -0300

    Paginated

--- client/src/components/CardsContainer/CardsContainer.js
+++ client/src/components/CardsContainer/CardsContainer.js
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getAllDogs } from '../../redux/action';
 import Card from '../Card/Card';
 import { Link } from 'react-router-dom';
+import Paginated from '../Paginated/Paginated';
 
 function CardsContainer() {
   const dogs = useSelector(state => state.dogs);
@@ -15,10 +16,26 @@ function CardsContainer() {
     dispatch(getAllDogs());
   }, [dispatch]);
 
+  // Paginated
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dogsPerPage, setDogsPerPage] = useState(8);
+  const indexOFLastDog = currentPage * dogsPerPage - 1; // (3*8)-1=23
+  const indexOfFirstDog = indexOFLastDog - (dogsPerPage - 1); // 23-7=16
+  const currentDogs = dogs.slice(indexOfFirstDog, indexOFLastDog + 1); // (0, 7)(8, 15)(16, 23)
+
+  const paginated = pageNumber => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
-      {dogs.length ? (
-        dogs.map(d => {
+      <Paginated
+        dogsPerPage={dogsPerPage}
+        totalDogs={dogs.length}
+        paginated={paginated}
+      />
+      {currentDogs.length ? (
+        currentDogs.map(d => {
           return (
             <Link to="#" key={d.id}>
               <Card
